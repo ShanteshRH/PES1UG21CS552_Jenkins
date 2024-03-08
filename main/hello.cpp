61e0cbaf9fc490074a9aa5a1c2edf75d30e6f4aa
Shantesh R Hosmath <shanteshrhosmath@gmail.com>
2024-03-08 12:32:44 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Added Webhooks\n";
+  cout << "Hello, Github\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
